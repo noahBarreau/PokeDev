cb4e73a59b34fd9dfd2e400c93140dd4faca8ca6
noahBarreau <n.barreau17@gmail.com>
2024-12-05 15:00:17 +0100

    origin

---
+++ src/Header.jsx
@@ -0,0 +1,14 @@
+const Header = () => {
+    return (
+      <header>
+        <nav>
+          <ul>
+            <li>Accueil</li>
+            <li>Blog</li>
+          </ul>
+        </nav>
+      </header>
+    );
+  };
+  
+  export default Header;
