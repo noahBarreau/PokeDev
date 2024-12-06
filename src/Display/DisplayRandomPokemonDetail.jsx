7f857f7c7761bfc1cd8db48a8ba4ec34bdcea356
noahBarreau <n.barreau17@gmail.com>
2024-12-06 15:30:16 +0100

    modif visuel

--- src/Display/DisplayRandomPokemonDetail.jsx
+++ src/Display/DisplayRandomPokemonDetail.jsx
@@ -1,18 +1,16 @@
 import Header from "../Header";
 import PokemonById from "../component/ComponentPokemonById";
-
 import '../css/style.css';
 
 function DisplayRandomPokemonDetail() {
   const id = Math.floor(Math.random() * 838) + 1;
 
   return (
-    <>
+    <div className="main-container"> {/* Ajout du conteneur principal */}
       <Header />
-      
-      <PokemonById id={id}/>
-    </>
-  )
+      <PokemonById id={id} />
+    </div>
+  );
 }
 
-export default DisplayRandomPokemonDetail
+export default DisplayRandomPokemonDetail;
